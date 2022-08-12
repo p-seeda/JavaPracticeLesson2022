40c69a44ca79d4ad803188b485bcdab12d5a8514
p-seeda <pseeda.p@gmail.com>
2022-08-12 15:58:47 -0500

    updated datas

--- JavaPractice/DemoTestClass.java
+++ JavaPractice/DemoTestClass.java
@@ -3,7 +3,10 @@ public class DemoTestClass {
 
 	public static void main(String[] args) {
 		System.out.println("This is a demo test class for testing");
-
+int x=10;
+int y = 20;
+int z = x+y;
+System.out.println("Sum of " + x + " and " + y + " = " + z   );
 	}
 
 }
---
+++ JavaPractice/NewPackageTest/NewPackageTest11.java
@@ -0,0 +1,10 @@
+package NewPackageTest;
+
+public class NewPackageTest11 {
+
+	public static void main(String[] args) {
+		System.out.println("This is another package testing");
+
+	}
+
+}
